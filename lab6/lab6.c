d1a4b89898d0538918f366a3556935d49415a818
Groysman881 <44917666+Groysman881@users.noreply.github.com>
2020-12-24 17:08:59 +0300

    Update lab6.c

--- lab6/lab6.c
+++ lab6/lab6.c
@@ -18,47 +18,47 @@ pthread_t tid[NUM_OF_THREADS];
 
 void* tWrite(void* arg){
  	while(1){
-     	pthread_rwlock_wrlock(&rwl);
-      	counter++;
-     	sprintf(buf,"%d",counter);
-    	strcat(data,buf);
-     	pthread_rwlock_unlock(&rwl);
-    	sleep(rand()%3+1);
+     		pthread_rwlock_wrlock(&rwl);
+      		counter++;
+     		sprintf(buf,"%d",counter);
+    		strcat(data,buf);
+     		pthread_rwlock_unlock(&rwl);
+    		sleep(rand()%3+1);
   	}
 }
 
 void* tRead(void* arg){
 	sleep(1);
-    while(1){
-        pthread_rwlock_rdlock(&rwl);
-        printf("tid of reading thread = %lx data from writing thread = %s\n",(long)pthread_self(),data);
-        pthread_rwlock_unlock(&rwl);
-        sleep(1);
+    	while(1){
+        	pthread_rwlock_rdlock(&rwl);
+        	printf("tid of reading thread = %lx data from writing thread = %s\n",(long)pthread_self(),data);
+        	pthread_rwlock_unlock(&rwl);
+        	sleep(1);
 	}
 }
 
 int main(int argc,char* argv[]){
 	srand(time(NULL));
-    data = (char*)malloc(150);
-    if(pthread_rwlock_init(&rwl,NULL) != 0){
-    	perror("rwlock init");
-    	exit(-1);
-    }
+    	data = (char*)malloc(150);
+    	if(pthread_rwlock_init(&rwl,NULL) != 0){
+    		perror("rwlock init");
+    		exit(-1);
+    	}
    	int create = pthread_create(tid,NULL,tWrite,NULL);
   	if(create != 0){
  		perror("create");
    		exit(-1);
-    }
+    	}
    	for(int i = 1;i < NUM_OF_THREADS;i++){
-    	create = pthread_create(tid+i,NULL,tRead,NULL);
-       	if(create != 0){
-        	perror("create");
-        	exit(-1);
+    		create = pthread_create(tid+i,NULL,tRead,NULL);
+       		if(create != 0){
+        		perror("create");
+        		exit(-1);
 		}
  	}
-    for(int i = 0;i < NUM_OF_THREADS;i++){
-    	pthread_join(tid[i],NULL);
-    }
+    	for(int i = 0;i < NUM_OF_THREADS;i++){
+    		pthread_join(tid[i],NULL);
+    	}
 	pthread_rwlock_destroy(&rwl);
 	return 0;
 }
